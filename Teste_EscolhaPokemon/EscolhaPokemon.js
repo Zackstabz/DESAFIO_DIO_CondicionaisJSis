b834e05f4b6cb15f6029705b3017c9f40dca6a44
Rodrigo "Zackstabz" Medeiros <63007829+Zackstabz@users.noreply.github.com>
2024-01-03 16:20:11 -0300

    Create EscolhaPokemon.js

---
+++ Teste_EscolhaPokemon/EscolhaPokemon.js
@@ -0,0 +1,23 @@
+// Entrada de dados. Lembre-se: O parseInt(()) é importante para a conversão dos valores de entrada(String) para um valor numérico(int).
+let escolhaDoTreinador = parseInt(gets());
+let pokemonEscolhido; 
+
+// Implementa as condições necessárias para a solução do desafio. Utilize a tabela de exemplos para identificar a escolha do treinador:
+if (escolhaDoTreinador === 1) {
+    pokemonEscolhido = "Bulbasaur";
+} else if (escolhaDoTreinador === 2) {
+    pokemonEscolhido = "Charmander";
+} else if (escolhaDoTreinador === 4) {
+    pokemonEscolhido = "Pikachu";
+} else if (escolhaDoTreinador === 5) {
+    pokemonEscolhido = "Mewtwo";
+} else {
+    pokemonEscolhido = null; // Valor nulo para escolhas inválidas
+}
+
+// Imprime o Pokémon escolhido:
+if (pokemonEscolhido) {
+    print("Voce escolheu o " + pokemonEscolhido + " como seu Pokemon inicial.");
+} else {
+    print("Escolha inválida. Por favor, escolha entre 1, 2, 4 ou 5.");
+}
